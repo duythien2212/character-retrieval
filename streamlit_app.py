b22d8716b1b45bc2b0162235d1b7880ab0cd93ba
duythien2212 <112065049+duythien2212@users.noreply.github.com>
2024-11-20 08:04:01 +0000

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,49 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# Sidebar
+with st.sidebar:
+    st.write("Select input image query")
+    
+    # Select movie
+    movies = ["Movie 1", "Movie 2"]
+    selected_movie = st.selectbox(
+        "Select movie",
+        movies,
+    )
+
+    # Select character 
+    characters = ["Character 1", "Character 2", "Character 3"]
+    selected_character = st.selectbox(
+        "Select character",
+        characters,
+    )
+
+    # Select face feature types
+    face_feature_types = ["Facenet", "Arcface"]
+    selected_feature_type = st.selectbox(
+        "Select face feature types",
+        face_feature_types,
+    )
+
+    # Select top_k result
+    top_k = 5
+    top_k = st.number_input("Select top_k result", min_value=1, value=top_k)
+
+
+# Main
+st.title("Movie character retrieval")
+st.write("Movie character retrieval based on image examples")
+
+img1 = st.image("https://static.streamlit.io/examples/cat.jpg", width=100),
+    
+options = [
+    img1,
+    img1,
+    img1
+    ]
+selection = st.segmented_control(
+    "Directions", options, selection_mode="multi"
 )
+st.markdown(f"Your selected options: {selection}.")
+
+
